073437e752351cb11cf78b3429701b235f146c50
alanwzyasl@gmail.com <alanwzyasl@gmail.com>
2023-01-27 03:38:15 +0000

    add new notebok

---
+++ My folder/Demo notebook (1).py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+print("Hello everyone")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Title1
+# MAGIC ## Title2
+# MAGIC ### Title3
+# MAGIC * Title4
+# MAGIC * Title5
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
